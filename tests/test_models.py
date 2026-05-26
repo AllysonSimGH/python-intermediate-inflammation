fb03b1a1cd2aa1056108227d286b0d5354d2dbf3
Allyson Sim <g.h.sim@tudelft.nl>
2026-05-26 15:37:18 +0200

    add more tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,31 +6,33 @@ import pytest
 
 from inflammation.models import daily_mean, daily_min, daily_max
 
-#@pytest.mark.parametrize(                       # python decorator
+# @pytest.mark.parametrize(                       # python decorator
 #        "test_input, test_result",                # name of arguments
 #        [
 #            ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),   # values of arguments (copy from existing tests below)
 #            ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
 #        ])
 
+
 @pytest.mark.parametrize(
-        "test_input, test_result",
-        [
-            ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
-            ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
-            (np.zeros((3, 5)), np.zeros(5)),
-            ([[1, 2, 3]], [1, 2, 3]),
-        ])
-
-
-def test_daily_mean(test_input, test_result): # add input arguments from the parametrize decorator
+    "test_input, test_result",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
+        (np.zeros((3, 5)), np.zeros(5)),
+        ([[1, 2, 3]], [1, 2, 3]),
+    ],
+)
+def test_daily_mean(
+    test_input, test_result
+):  # add input arguments from the parametrize decorator
     """Test that mean function works for an array of zeros and positive integers."""
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
 
 # def test_daily_mean_zeros():
 #     """Test that mean function works for an array of zeros."""
-    
+
 
 #     test_input = np.array([[0, 0],
 #                            [0, 0],
@@ -41,51 +43,77 @@ def test_daily_mean(test_input, test_result): # add input arguments from the par
 #     npt.assert_array_equal(daily_mean(test_input), test_result)
 
 
-#def test_daily_mean_integers():
-    # """Test that mean function works for an array of positive integers."""
+# def test_daily_mean_integers():
+# """Test that mean function works for an array of positive integers."""
 
-    # test_input = np.array([[1, 2],
-    #                        [3, 4],
-    #                        [5, 6]])
-    # test_result = np.array([3, 4])
+# test_input = np.array([[1, 2],
+#                        [3, 4],
+#                        [5, 6]])
+# test_result = np.array([3, 4])
 
-    # # Need to use Numpy testing functions to compare arrays
-    # npt.assert_array_equal(daily_mean(test_input), test_result)
+# # Need to use Numpy testing functions to compare arrays
+# npt.assert_array_equal(daily_mean(test_input), test_result)
+
 
 def test_daily_mean_string():
-    """Test for TypeError when passing strings"""      # Write summary before code clarifies purpose
+    """Test for TypeError when passing strings"""  # Write summary before code clarifies purpose
 
-    with pytest.raises(TypeError):                     # string instead of array give TypeError
-        error_expected = daily_mean(['hi', 'there'])    # simple input  
+    with pytest.raises(TypeError):  # string instead of array give TypeError
+        error_expected = daily_mean(["hi", "there"])  # simple input
 
 
 @pytest.mark.parametrize(
-        "test_input, test_result",
-        [
-            ([[1, 2], [3, 4], [5, 6]], [5, 6]),
-            ([[1, 2, -9], [-3, 4, -2], [-1, 5, -6]], [1, 5, -2]),
-        ])
-
+    "test_input, test_result",
+    [
+        ([[1, 2], [3, 4], [5, 6]], [5, 6]),
+        ([[1, 2, -9], [-3, 4, -2], [-1, 5, -6]], [1, 5, -2]),
+    ],
+)
 def test_daily_max(test_input, test_result):
     """Test that max function works for an array of positive and negative integers."""
     npt.assert_array_equal(daily_max(test_input), test_result)
 
 
-#def test_daily_max_integers():
-    # """Test that the max function works for an array of positive and negative integers.
-    # """
-    
-    # test_input = np.array([[1, 2, -9], [-3, 4, -2], [-1, 5, -6]])
-    # test_result = np.array([1, 5, -2])              # update test_output accordingly
+@pytest.mark.parametrize(
+    "test_input, test_result",
+    [
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [0, 0, 0]),
+        ([[1, 2, -1], [3, -2, 4], [5, -9, 6]], [1, -9, -1]),
+        ([[0, 1, 2], [0, 3, 4]], [0, 1, 2]),  # array containing zeros
+        ([[3, 3, 3], [3, 3, 3], [3, 3, 3]], [3, 3, 3]),  # all values the same
+    ],
+)
+def test_daily_min(test_input, test_result):
+    """Test that min function works for an array of positive and negative integers."""
+    npt.assert_array_equal(daily_min(test_input), test_result)
 
 
-    # # Need to use Numpy testing functions to compare arrays
-    # npt.assert_array_equal(daily_max(test_input), test_result) 
+def test_daily_max_empty_array():
+    """Test that daily_max raises ValueError when given an empty array."""
+    with pytest.raises(ValueError):
+        daily_max([])
 
 
-#def test_daily_max_string():
-    # """Test for TypeError when passing strings"""      # Write summary before code clarifies purpose
+def test_daily_max_nan_propagation():
+    data = np.array([[1, np.nan], [3, 4]])
+    result = daily_max(data)
+    assert np.isnan(result[1])  # documents current behavior
 
-    # with pytest.raises(TypeError):                     # string instead of array give TypeError
-    #     error_expected = daily_max(['hi', 'there'])    # simple input      
 
+# def test_daily_max_integers():
+# """Test that the max function works for an array of positive and negative integers.
+# """
+
+# test_input = np.array([[1, 2, -9], [-3, 4, -2], [-1, 5, -6]])
+# test_result = np.array([1, 5, -2])              # update test_output accordingly
+
+
+# # Need to use Numpy testing functions to compare arrays
+# npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+# def test_daily_max_string():
+# """Test for TypeError when passing strings"""      # Write summary before code clarifies purpose
+
+# with pytest.raises(TypeError):                     # string instead of array give TypeError
+#     error_expected = daily_max(['hi', 'there'])    # simple input
